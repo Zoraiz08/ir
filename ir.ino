d877dd6027bbfa56c78055b1a54a2f32820c4363
Zoraiz <zoraizmahmood14@gmail.com>
2025-04-29 12:46:04 +0200

    ir-coche

--- ir.ino
+++ ir.ino
@@ -1,14 +1,81 @@
+//SENSOR
 const int IR = 3;
 
+//MOTOR DRETA
+const int IN1 = 3;
+const int IN2 = 5;
+
+//MOTOR ESQUERRA
+const int IN3 = 9;
+const int IN4 = 10;
+
+//ON/OFF
+const int EEP = 4;
 
 void setup() {
-  pinMode(IR, INPUT);
-  Serial.begin(9600);
+//PIN DEL MOTORS --> SORTIDA
+pinMode(IN1, OUTPUT);
+pinMode(IN2, OUTPUT);
+pinMode(IN3, OUTPUT);
+pinMode(IN4, OUTPUT);
+pinMode(EEP, OUTPUT);
+
+//PIN DEL SENSOR --> ENTRADA
+pinMode(IR, INPUT);
+
+//DRIVER --> ON
+digitalWrite(EEP, HIGH);
 
 }
 
 void loop() {
-  int valor = digitalRead(IR);
-  Serial.println(valor);
 
+
+}
+
+void up () {
+//motro dreta
+digitalWrite(IN1, HIGH);
+digitalWrite(IN2, LOW);
+//motor esquerra
+digitalWrite(IN3, HIGH);
+digitalWrite(IN4, LOW);
+}
+
+void down (){
+//motro dreta
+digitalWrite(IN1, LOW);
+digitalWrite(IN2, HIGH);
+//motor esquerra
+digitalWrite(IN3, LOW);
+digitalWrite(IN4, HIGH);
+}
+
+void right (){
+//motro dreta
+digitalWrite(IN1, LOW);
+digitalWrite(IN2, HIGH);
+//motor esquerra
+digitalWrite(IN3, LOW);
+digitalWrite(IN4, LOW);
+
+}
+void left (){
+//motro dreta
+digitalWrite(IN1, LOW);
+digitalWrite(IN2, LOW);
+//motro izquierda
+digitalWrite(IN3, LOW);
+digitalWrite(IN4, HIGH);
+
+}
+
+void stop(){
+
+//motro dreta
+digitalWrite(IN1, LOW);
+digitalWrite(IN2, LOW);
+//motor esquerra
+digitalWrite(IN3, LOW);
+digitalWrite(IN4, LOW);
 }
