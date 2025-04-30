1b974b7bffedd75d769960cca4daaf2ee4a934f8
Zoraiz08 <zoraizmahmood14@gmail.com>
2025-04-30 10:21:24 +0200

    Update ir.ino

--- ir.ino
+++ ir.ino
@@ -13,7 +13,7 @@ const int IN4 = 10;
 //ON/OFF
 const int EEP = 4;
 
-void setup() {
+void setup(){
 //PIN DEL MOTORS --> SORTIDA
 pinMode(IN1, OUTPUT);
 pinMode(IN2, OUTPUT);
@@ -29,27 +29,7 @@ digitalWrite(EEP, HIGH);
 
 }
 
-void loop() {
-int VALOR_D = digitalRead(IR_D);
-int VALOR_E = digitalRead(IR_E);
-
-if (comparar(VALOR_D, VALOR_E) == "right"){
-  right();
-}else if (comparar(VALOR_D, VALOR_E) == "left"){
-  left();
-}else{
-  up();
-}
-
-string comparar(int VALOR_D,int VALOR_E){
-  if (VALOR_D == 1 || VALOR_E == 0){
-    return "right";
-  }else if (VALOR_D == 0 || VALOR_E == 1){
-    return "left";
-
-}
-
-void up () {
+void up(){
 //motro dreta
 digitalWrite(IN1, HIGH);
 digitalWrite(IN2, LOW);
@@ -58,7 +38,7 @@ digitalWrite(IN3, HIGH);
 digitalWrite(IN4, LOW);
 }
 
-void down (){
+void down(){
 //motro dreta
 digitalWrite(IN1, LOW);
 digitalWrite(IN2, HIGH);
@@ -67,7 +47,7 @@ digitalWrite(IN3, LOW);
 digitalWrite(IN4, HIGH);
 }
 
-void right (){
+void right(){
 //motro dreta
 digitalWrite(IN1, LOW);
 digitalWrite(IN2, HIGH);
@@ -76,22 +56,41 @@ digitalWrite(IN3, LOW);
 digitalWrite(IN4, LOW);
 
 }
-void left (){
+void left(){
 //motro dreta
 digitalWrite(IN1, LOW);
 digitalWrite(IN2, LOW);
 //motro izquierda
 digitalWrite(IN3, LOW);
 digitalWrite(IN4, HIGH);
-
 }
 
 void stop(){
-
 //motro dreta
 digitalWrite(IN1, LOW);
 digitalWrite(IN2, LOW);
 //motor esquerra
 digitalWrite(IN3, LOW);
 digitalWrite(IN4, LOW);
+}
+
+void loop(){
+int VALOR_D = digitalRead(IR_D);
+int VALOR_E = digitalRead(IR_E);
+
+if (comparar(VALOR_D, VALOR_E) == "r"){
+  right();
+}else if (comparar(VALOR_D, VALOR_E) == "l"){
+  left();
+}else{
+  up();
+  }
+}
+char comparar(int VALOR_D,int VALOR_E){
+  if (VALOR_D == 1 || VALOR_E == 0){
+    return "r";
+  }else if (VALOR_D == 0 || VALOR_E == 1){
+    return "l";
+  }
+
 }
