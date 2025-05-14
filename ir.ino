c99d9b1a1b92521a5922e08be7d7dc1476967272
Zoraiz08 <zoraizmahmood14@gmail.com>
2025-05-14 10:50:54 +0200

    Update ir.ino

--- ir.ino
+++ ir.ino
@@ -3,8 +3,8 @@ const int IR_D = 6;
 const int IR_E = 7;
 
 //MOTOR DRETA
-const int IN1 = 3;
-const int IN2 = 5;
+const int IN1 = 5;
+const int IN2 = 3;
 
 //MOTOR ESQUERRA
 const int IN3 = 9;
@@ -27,70 +27,74 @@ pinMode(IR_E, INPUT);
 //DRIVER --> ON
 digitalWrite(EEP, HIGH);
 
+Serial.begin(9600);
+
 }
+int velocidad = 170;
 
 void up(){
 //motro dreta
-digitalWrite(IN1, HIGH);
-digitalWrite(IN2, LOW);
+analogWrite(IN1, velocidad);
+analogWrite(IN2, 0);
 //motor esquerra
-digitalWrite(IN3, HIGH);
-digitalWrite(IN4, LOW);
+analogWrite(IN3, velocidad);
+analogWrite(IN4, 0);
 }
 
 void down(){
 //motro dreta
-digitalWrite(IN1, LOW);
-digitalWrite(IN2, HIGH);
+analogWrite(IN1, 0);
+analogWrite(IN2, velocidad);
 //motor esquerra
-digitalWrite(IN3, LOW);
-digitalWrite(IN4, HIGH);
+analogWrite(IN3, 0);
+analogWrite(IN4, velocidad);
 }
 
 void right(){
 //motro dreta
-digitalWrite(IN1, LOW);
-digitalWrite(IN2, HIGH);
+analogWrite(IN1, 0);
+analogWrite(IN2, velocidad);
 //motor esquerra
-digitalWrite(IN3, LOW);
-digitalWrite(IN4, LOW);
+analogWrite(IN3, 0);
+analogWrite(IN4, 0);
 
 }
 void left(){
 //motro dreta
-digitalWrite(IN1, LOW);
-digitalWrite(IN2, LOW);
+analogWrite(IN1, 0);
+analogWrite(IN2, 0);
 //motro izquierda
-digitalWrite(IN3, LOW);
-digitalWrite(IN4, HIGH);
+analogWrite(IN3, 0);
+analogWrite(IN4, velocidad);
 }
 
 void stop(){
 //motro dreta
-digitalWrite(IN1, LOW);
-digitalWrite(IN2, LOW);
+analogWrite(IN1, 0);
+analogWrite(IN2, 0);
 //motor esquerra
-digitalWrite(IN3, LOW);
-digitalWrite(IN4, LOW);
+analogWrite(IN3, 0);
+analogWrite(IN4, 0);
 }
 
 void loop(){
 int VALOR_D = digitalRead(IR_D);
 int VALOR_E = digitalRead(IR_E);
 
-if (comparar(VALOR_D, VALOR_E) == "r"){
+Serial.print("E:");
+Serial.print(VALOR_E);
+Serial.print(" d:");
+Serial.print(VALOR_D);
+
+if (VALOR_D > VALOR_E){
+
   right();
-}else if (comparar(VALOR_D, VALOR_E) == "l"){
+  Serial.println("r");
+}else if (VALOR_D < VALOR_E){
   left();
+  Serial.println("l");
 }else{
   up();
+  Serial.println("u");
   }
-}
-char comparar(int VALOR_D,int VALOR_E){
-  if (VALOR_D == 1 && VALOR_E == 0){
-    return "r";
-  }else if (VALOR_D == 0 && VALOR_E == 1){
-    return "l";
-  }
-
 }
